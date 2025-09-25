dcb6037c0d9225272a70e867733d543a31dc7a89
turcos69 <andrea.turco@salveminialessano.edu.it>
2025-09-25 12:44:32 +0200

    update 2 commit
    aggiungo la differenza e sistemo il programma

--- LABORATORIO/uno.cpp
+++ LABORATORIO/uno.cpp
@@ -5,8 +5,11 @@ int main()
 {
     int num1 = 8;
     int num2 = 5;
-    int somma = num1-num2;
-    cout <<"la somma dei due numeri è " << somma;
+    int somma = num1+num2;
+    int differenza = num1-num2;
+    cout <<"la somma dei due numeri è " << somma << endl;     
+	cout <<"la differenza dei due numeri è " << differenza; 
+ 
 
     return 0;
 }
