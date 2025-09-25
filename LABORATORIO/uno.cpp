6b868ac9733902932841d67d5e2075926a0e87ee
turcos69 <andrea.turco@salveminialessano.edu.it>
2025-09-25 12:38:48 +0200

    create 1 commit
    dati 2 numeri calcola la somma

---
+++ LABORATORIO/uno.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1 = 8;
+    int num2 = 5;
+    int somma = num1-num2;
+    cout <<"la somma dei due numeri è " << somma;
+
+    return 0;
+}
